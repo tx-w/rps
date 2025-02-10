18c723f055abcd324bc6809e784b1cc90c18b634
tx-w <7542400+tx-w@users.noreply.github.com>
2025-02-09 16:10:19 -0800

    created global variables and wrote functions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
 let x = 1
+humanScore = 0
+computerScore = 0
 
-x = Math.random() * x
-
-
-function getComputerChoice(y) {
-
+function getComputerChoice() {
+    x = Math.random() * x;
+    if (x < .33) {
+        return "rock"
+    }
+    else if (x > .33 && x < .66){
+        return "paper"
+    }
+    else if (x > .66) {
+        return "scissors"
+    }
 }
 
 function getHumanChoice () {
+    let game = prompt("jan ken pon!")
+    return game
+} 
 
-}
+console.log(getHumanChoice())
 
